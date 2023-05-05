228a2d9c9d1852c4dab6588d36d500611a0484b3
Jokerfukc <121379190+Jokerfukc@users.noreply.github.com>
2023-05-05 18:22:33 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -50,7 +50,99 @@ def num8():
         print('Невыполнимое условие')
 
 
-print("Введите номер задачи (2/4/6/8)")
+def num10():
+    n = int(input())
+    count_zero = 0
+    count_one = 0
+    for i in range(n):
+        x = int(input())
+    if x == 0:
+        count_zero += 1
+    else:
+        count_one += 1
+    if count_one > count_zero:
+        print(count_zero)
+    else:
+        print(count_one)
+
+
+def num12():
+    x = int(input())
+    y = int(input())
+    for i in range(x):
+        for j in range(y):
+            if x == i + j and y == i * j:
+                print(i, j)
+
+
+def num14():
+    n = int(input())
+    i = 0
+    while 2 ** i <= n:
+        print(2 ** i)
+    i += 1
+
+
+def num16():
+    N = abs(int(input('Введите количество элементов списка А: ')))
+    A_entered = input("Введите через пробел элементы списка: ").split()
+    A_num = list(map(int, A_entered))
+    if len(A_num) != N:
+        print('Введенные элементы не соответствуют заявленному количеству!')
+    else:
+        X = int(input('Введите число X, которое необходимо найти в списке: '))
+        count = 0
+        for i in range(N):
+            if A_num[i] == X:
+                count += 1
+        print(f'Число {X} встречается в списке A {count} раз')
+
+
+def num18():
+    N = abs(int(input('Введите количество элементов списка А: ')))
+    A_entered = input("Введите через пробел элементы списка: ").split()
+    A_num = list(map(int, A_entered))
+    if len(A_num) != N or N == 0:
+        print('Введенные элементы не соответствуют заявленному количеству!')
+    else:
+        X = int(input('Введите число X, с которым необходимо сравнивать элементы списка: '))
+        min = abs(X - A_num[0])
+        index = 0
+        for i in range(1, N):
+            count = abs(X - A_num[i])
+            if count < min:
+                min = count
+                index = i
+        print(
+            f'Число {A_num[index]} в списке A наиболее близко по величине к числу {X},'
+            f' их разница составляет {abs(X - A_num[index])}')
+
+
+def num20():
+    eng = {1: 'AEIOULNSTR',
+           2: 'DG',
+           3: 'BCMP',
+           4: 'FHVWY',
+           5: 'K',
+           8: 'JZ',
+           10: 'QZ'}
+    rus = {1: 'АВЕИНОРСТ',
+           2: 'ДКЛМПУ',
+           3: 'БГЁЬЯ',
+           4: 'ЙЫ',
+           5: 'ЖЗХЦЧ',
+           8: 'ШЭЮ',
+           10: 'ФЩЪ'}
+    N = abs(int(input('Введите 1, если играем в английской раскладке, либо 0, если в русской: ')))
+    word = input('Введите слово: ').upper()
+    if N == 1:
+        print('За это слово вы получаете', sum([k for i in word for k, v in eng.items() if i in v]), 'очков')
+    elif N == 0:
+        print('За это слово вы получаете', sum([k for i in word for k, v in rus.items() if i in v]), 'очков')
+    else:
+        print('Вы мухлюете и играете не по правилам!')
+
+print("Введите номер задачи (2/4/6/8/10/12/14/16/18/20)")
 x = int(input())
 if x == 2:
     num2()
@@ -60,5 +152,17 @@ elif x == 6:
     num6()
 elif x == 8:
     num8()
+elif x == 10:
+    num10()
+elif x == 12:
+    num12()
+elif x == 14:
+    num14()
+elif x == 16:
+    num16()
+elif x == 18:
+    num18()
+elif x == 20:
+    num20()
 else:
     print("Задача не найдена")
